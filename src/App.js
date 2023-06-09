900924709a260acc1427973640a125b36f756bc8
Seval Senturk <seval.senturk@tgs.aero>
2023-06-09 13:54:16 +0300

    form ornek finished

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
+import PersonalInfo from './PersonalInfo';
 
 function App() {
   return (
     <div className="App">
-        
+      <PersonalInfo />
     </div>
   );
 }
 
 export default App;
+
